f48ffc98f4fe729154d601a0478e5cec163f5233
Arnab Chakraborty <that.zytact@gmail.com>
2025-06-06 01:11:14 +0530

    feat: add prettier-plugin-tailwindcss for improved Tailwind CSS formatting

--- prettier.config.js
+++ prettier.config.js
@@ -5,10 +5,11 @@
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 export default config;
